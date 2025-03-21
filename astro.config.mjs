edec05cf6bb01fcd940e524d3505d606ed37eaf0
adarlpz <adarlopz.dev@gmail,com>
2025-03-21 10:44:52 -0600

    correccion url global|

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tekkure.com/tekkure/web/v2/dist',  // Define la URL base
+});
