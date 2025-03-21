119ab595df0236fa75939597eb7ccc6347125190
adarlpz <adarlopz.dev@gmail,com>
2025-03-21 11:13:44 -0600

    arregle el server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tekkure.com/tekkure/web/v2/dist',  // Define la URL base
+    site: 'https://tekkure.com/dist',  // Define la URL base
 });
